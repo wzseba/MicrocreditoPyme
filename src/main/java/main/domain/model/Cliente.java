1eb2e7620b1cd22e5dbba56e1375d41b20ff5183
Sebastian <wzseba@gmail.com>
2025-08-10 22:38:22 -0300

    add: constructor

--- src/main/java/main/domain/model/Cliente.java
+++ src/main/java/main/domain/model/Cliente.java
@@ -4,7 +4,19 @@ import java.util.UUID;
 
 public class Cliente {
 
-	private UUID id;
+	private final UUID id;
 	private String nombreEmpresa;
 	private double ingresoAnuales;
+
+	public Cliente(String nombreEmpresa, double ingresoAnuales) {
+		this.id = UUID.randomUUID();
+		this.nombreEmpresa = nombreEmpresa;
+		this.ingresoAnuales = ingresoAnuales;
+	}
+
+	public Cliente(UUID id, String nombreEmpresa, double ingresoAnuales) {
+		this.id = id;
+		this.nombreEmpresa = nombreEmpresa;
+		this.ingresoAnuales = ingresoAnuales;
+	}
 }
