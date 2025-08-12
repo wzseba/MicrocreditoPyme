905f66e30f33871fdec3959d9b25f9e3ff98552c
Sebastian <wzseba@gmail.com>
2025-08-11 22:18:12 -0300

    add: capacidad de endeudamiento

--- src/main/java/main/domain/model/Cliente.java
+++ src/main/java/main/domain/model/Cliente.java
@@ -1,22 +1,28 @@
 package main.domain.model;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public class Cliente {
 
 	private final UUID id;
 	private String nombreEmpresa;
-	private double ingresoAnuales;
+	private BigDecimal ingresoAnuales;
 
-	public Cliente(String nombreEmpresa, double ingresoAnuales) {
+	public Cliente(String nombreEmpresa, BigDecimal ingresoAnuales) {
 		this.id = UUID.randomUUID();
 		this.nombreEmpresa = nombreEmpresa;
 		this.ingresoAnuales = ingresoAnuales;
 	}
 
-	public Cliente(UUID id, String nombreEmpresa, double ingresoAnuales) {
+	public Cliente(UUID id, String nombreEmpresa, BigDecimal ingresoAnuales) {
 		this.id = id;
 		this.nombreEmpresa = nombreEmpresa;
 		this.ingresoAnuales = ingresoAnuales;
+	}
+
+	public BigDecimal calcularCapacidadEndeudamiento() {
+		// Regla: como maximo 40% de ingresos anuales
+		return this.ingresoAnuales.multiply(new BigDecimal("0.4"));
 	}
 }
